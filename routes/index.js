9726cc30218cd89a5c2b7e75cbea9b63f4bef314
Alexander Polubnev <alexpolubnev8@gmail.com>
2020-02-13 11:43:00 +0300

    add index and login routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.redirect('/entries');
+  res.redirect('/main');
 });
 
 module.exports = router;
--- routes/registr.js
+++ routes/registr.js
@@ -3,20 +3,20 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.render('registr/registr');
-});
+// router.get('/', async (req, res, next) => {
+//   res.render('registr/registr');
+// });
 
-router.post('/add', async (req, res, next) => {
-  const { login, email, password } = req.body;
-  try {
-    const user = new User ({ login, email, password });
-    await user.save();
-    req.session.user = user._id;
-    res.redirect('/entries');
-  } catch (e) {
-    const notLogined = 'Пользователь с таким именем или почтой существуют';
-    res.render('registr/registr', { notLogined });
-  }
-});
+// router.post('/add', async (req, res, next) => {
+//   const { login, email, password } = req.body;
+//   try {
+//     const user = new User ({ login, email, password });
+//     await user.save();
+//     req.session.user = user._id;
+//     res.redirect('/entries');
+//   } catch (e) {
+//     const notLogined = 'Пользователь с таким именем или почтой существуют';
+//     res.render('registr/registr', { notLogined });
+//   }
+// });
 module.exports = router;
